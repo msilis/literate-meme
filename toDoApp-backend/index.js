793b32efafa2a2086b360911b47b75e17d4305ec
msilis <mikssilis@gmail.com>
2023-07-12 22:45:31 +0100

    strict query

--- toDoApp-backend/index.js
+++ toDoApp-backend/index.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const bodyParser = require("body-parser");
 const port = process.env.port || 3001;
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL);
-mongoose.set('strictQuery', true);
+/* mongoose.set('strictQuery', true); */
 const db = mongoose.connection;
-db.on('error', (error)=>console.error(error));
-db.once('open', ()=>console.log('Connected to database'))
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
-app.use(express.json())
+app.use(express.json());
 app.use(bodyParser.json());
 
-const toDoRouter = require('./routes/toDoRoutes');
-app.use('/todo', toDoRouter)
+const toDoRouter = require("./routes/toDoRoutes");
+app.use("/todo", toDoRouter);
 
-app.listen(port,()=> console.log(`Listening on ${port}`))
+app.listen(port, () => console.log(`Listening on ${port}`));
