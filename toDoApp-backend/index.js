ba41a5896f79fd10ddd0fe0d88f9caa8d44a5891
msilis <mikssilis@gmail.com>
2023-07-16 07:40:57 +0100

    env and start script update

--- toDoApp-backend/index.js
+++ toDoApp-backend/index.js
@@ -6,7 +6,7 @@ const port = process.env.port || 3001;
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL);
-/* mongoose.set('strictQuery', true); */
+mongoose.set("strictQuery", true);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
