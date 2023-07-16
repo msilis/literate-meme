3e2bc2f72147366d0dca6a59f596ceb603682865
msilis <mikssilis@gmail.com>
2023-07-16 14:37:54 +0100

    cors options

--- toDoApp-backend/index.js
+++ toDoApp-backend/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const port = process.env.port || 3001;
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.connect(process.env.DATABASE_URL);
 mongoose.set("strictQuery", true);
@@ -11,6 +12,11 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
 
+//Cors options
+const corsOptions = {
+  origin: ["https://superb-marshmallow-50848a.netlify.app"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 
